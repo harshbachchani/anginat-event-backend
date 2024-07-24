1a03dc8200bcf439a8bf5d874fac51e98d56f68f
harshbachchani <harshbachchani9900@gmail.com>
2024-07-24 23:45:20 +0530

    updated

--- src/services/dateconversion.service.js
+++ src/services/dateconversion.service.js
@@ -1,7 +1,8 @@
 import { formatInTimeZone } from "date-fns-tz";
 
 const convertDateToIST = (date) => {
-  return formatInTimeZone(date, "Asia/Kolkata", "dd MMM yyyy HH:mm:ss");
+  return formatInTimeZone(date, "Asia/Kolkata", "yyyy-MM-dd HH:mm");
 };
 
+console.log(convertDateToIST(new Date()));
 export { convertDateToIST };
